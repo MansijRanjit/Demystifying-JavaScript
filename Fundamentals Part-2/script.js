96989d9cf3c8429e0f05ae6eb33b135c7b7e5dc9
Mansij Ranjit <mansijranjit@gmail.com>
2023-08-30 14:18:13 +0545

    Day 6

---
+++ Fundamentals Part-2/script.js
@@ -0,0 +1,23 @@
+/*
+//////////////////////(1) Activating strict mode////////////////////////
+"use strict";
+
+let hasDriversLiscence = false;
+const passTest = true;
+
+if (passTest) hasDriversLiscence = true;
+if (hasDriversLiscence) console.log("I can Drive! 🚗");
+
+//const interface = "Audio";
+//const private = "Mansij";
+*/
+
+//////////////////////(2) Functions////////////////////////
+
+function logger() {
+  console.log("My name is Mansij");
+}
+
+// calling/ running/ invoking function
+logger();
+logger();
