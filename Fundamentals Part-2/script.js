b5b8dde10875a5190895e35ed3b3aaaa9e18e697
Mansij Ranjit <mansijranjit@gmail.com>
2023-08-31 09:29:02 +0545

    Day7

--- Fundamentals Part-2/script.js
+++ Fundamentals Part-2/script.js
@@ -12,6 +12,7 @@ if (hasDriversLiscence) console.log("I can Drive! 🚗");
 //const private = "Mansij";
 */
 
+/*
 //////////////////////(2) Functions////////////////////////
 
 function logger() {
@@ -21,3 +22,33 @@ function logger() {
 // calling/ running/ invoking function
 logger();
 logger();
+
+function fruitProcessor(mango, banana) {
+  console.log(mango, banana);
+
+  const juice = `Juice with ${mango} mangoes and ${banana} bananas`;
+  return juice;
+}
+const mixture = fruitProcessor(9, 7);
+console.log(mixture);
+
+console.log(fruitProcessor(1, 3));
+
+console.log(fruitProcessor("eight", "nine"));
+*/
+
+//////////////////////(3) Function Declarations vs Expressions////////////////////////
+
+//Function Declaration
+function calcAge1(currentYear, birthYear) {
+  return currentYear - birthYear;
+}
+const age1 = calcAge1(2023, 2001);
+console.log(age1);
+
+//Function Expression
+const calcAge2 = function calcAge2(currentYear, birthYear) {
+  return currentYear - birthYear;
+};
+const age2 = calcAge2(2023, 2012);
+console.log(age2);
