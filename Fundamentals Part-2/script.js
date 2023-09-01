0b7f65552b98922308dde8cec5e45901176f3679
Mansij Ranjit <mansijranjit@gmail.com>
2023-09-01 20:19:24 +0545

    Day 7

--- Fundamentals Part-2/script.js
+++ Fundamentals Part-2/script.js
@@ -13,7 +13,7 @@ if (hasDriversLiscence) console.log("I can Drive! 🚗");
 */
 
 /*
-//////////////////////(2) Functions////////////////////////
+//////////////////////(2) Functions ///////////////////////////
 
 function logger() {
   console.log("My name is Mansij");
@@ -104,7 +104,8 @@ const fruitProcessor = function (apples, oranges) {
 console.log(fruitProcessor(7, 9));
 */
 
-////////////////////// Challenge ////////////////////////
+/*
+////////////////////// Challenge ///////////////////////////////
 
 // Coding Challenge #1
 // Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
@@ -143,3 +144,93 @@ const checkWinner = function (avgDolphins, avgKoalas) {
   }
 };
 checkWinner(dolphinsAverage, koalasAverage);
+*/
+
+/*
+////////////////////////// (6) Arrays ///////////////////////////////////
+
+const friends = ["Ram", "Shyam", "Hari"];
+console.log(friends);
+
+const years = new Array(1990, 2000, 2010, 2020);
+
+console.log(friends[0]);
+console.log(years[2]);
+
+console.log(friends.length); //length of array
+console.log(friends[friends.length - 1]); //last element of array
+friends[2] = "Anbhimanyu";
+friends[3] = "Arjun";
+console.log(friends);
+
+const firstName = "Mansij";
+const mansij = [firstName, "Ranjit", 2023 - 2001, "Developer", friends];
+console.log(mansij);
+
+//Add elements in an array
+const people = ["Hamid", "Akash", "Michale", "Kevin"];
+const number = people.push("Arushi"); //adds element in the end of array and returns size of new array
+console.log(people);
+console.log(number);
+
+people.unshift("Jayant"); //adds element in the beginning of array and returns size of new array
+console.log(people);
+
+//Remove elements in an array
+people.pop(); //Removes last element and returns the element
+console.log(people);
+const popedElement = people.pop();
+console.log(popedElement);
+
+people.shift(); //Removes first element and returns the element
+console.log(people);
+
+console.log(people.indexOf("Akash"));
+console.log(people.indexOf("Akriti"));
+
+console.log(people.includes("Akash"));
+console.log(people.includes("Akriti"));
+
+if (people.includes("Hamid")) {
+  console.log(`You know the people named Hamid`);
+}
+*/
+
+/*
+////////////////////// Challenge ///////////////////////////////
+
+//Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array)
+
+const calcTip = function (billValue) {
+  if (billValue >= 50 && billValue <= 300) {
+    return billValue * 0.15;
+  } else {
+    return billValue * 0.2;
+  }
+};
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, total);
+*/
+
+////////////////////////// (7) Objects ///////////////////////////////////
+
+const intro = {
+  firstName: "Mansij",
+  lastName: "Ranjit",
+  age: 2023 - 2001,
+  job: "Developer",
+  friends: ["Ram", "Shyam", "Hari"],
+};
+console.log(intro);
