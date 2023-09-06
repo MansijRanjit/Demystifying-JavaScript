688b45bbee9a781be450fc20a4a6068c0ef80e5f
Mansij Ranjit <mansijranjit@gmail.com>
2023-09-06 22:51:43 +0545

    Day 12

--- (6)Modal Webpage/script.js
+++ (6)Modal Webpage/script.js
@@ -9,7 +9,7 @@ const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
 const openModal = function () {
-  console.log("Button clicked");
+  //console.log("Button clicked");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -25,3 +25,12 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+//Pressing Esc will close the modal
+document.addEventListener("keydown", function (e) {
+  //console.log(e);
+  //console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
