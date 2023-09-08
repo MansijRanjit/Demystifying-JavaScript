d5d51b1edee72e4316e8b32aa5a7b1825c7c2357
Mansij Ranjit <mansijranjit@gmail.com>
2023-09-08 11:10:25 +0545

    Day 13

--- (7)Pig Game/script.js
+++ (7)Pig Game/script.js
@@ -23,34 +23,67 @@ diceEl.classList.add("hidden");
 let currentScore = 0;
 const score = [0, 0];
 let activePlayer = 0;
+let playing = true;
+
+//Switching Player function
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  // document
+  //   .querySelector(`.player--${activePlayer}`)
+  //   .classList.remove("player--active");
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // document
+  //   .querySelector(`.player--${activePlayer}`)
+  //   .classList.add("player--active");
+  player0El.classList.toggle("player--active"); //toggle adds specified class if its not present and if it is present it removes it
+  player1El.classList.toggle("player--active");
+};
 
 //Rolling Dice Functionality
 btnRoll.addEventListener("click", function () {
-  //(1) Generating Random Dice Roll
-  let randDiceNo = Math.trunc(Math.random() * 6) + 1;
-  //console.log(randDiceNo);
+  if (playing) {
+    //(1) Generating Random Dice Roll
+    let randDiceNo = Math.trunc(Math.random() * 6) + 1;
+    //console.log(randDiceNo);
 
-  //(2) Displaying Dice
-  diceEl.src = `dice-${randDiceNo}.png`;
-  diceEl.classList.remove("hidden");
+    //(2) Displaying Dice
+    diceEl.src = `dice-${randDiceNo}.png`;
+    diceEl.classList.remove("hidden");
 
-  //(3) Check for rolled 1
-  if (randDiceNo !== 1) {
-    currentScore += randDiceNo;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    // document
-    //   .querySelector(`.player--${activePlayer}`)
-    //   .classList.remove("player--active");
+    //(3) Check for rolled 1
+    if (randDiceNo !== 1) {
+      currentScore += randDiceNo;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //Switch to next player
+      switchPlayer();
+    }
+  }
+});
 
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    // document
-    //   .querySelector(`.player--${activePlayer}`)
-    //   .classList.add("player--active");
-    player0El.classList.toggle("player--active"); //toggle adds specified class if its not present and if it is present it removes it
-    player1El.classList.toggle("player--active");
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    //1) Add current score to the active player's score
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+
+    //2) Check if player's score is >=100
+    if (score[activePlayer] >= 20) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+      diceEl.classList.add("hidden");
+    } else {
+      //3) Switch to next player
+      switchPlayer();
+    }
   }
 });
